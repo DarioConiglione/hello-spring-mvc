880a361eee848fb2f88988c73836655595fccda0
DarioConiglione <dariocng13@gmail.com>
2026-03-16 18:05:41 +0100

    Play with thymeleaf

--- src/main/java/org/lessons/java/hello_spring_mvc/controller/HomeController.java
+++ src/main/java/org/lessons/java/hello_spring_mvc/controller/HomeController.java
@@ -1,8 +1,12 @@
 package org.lessons.java.hello_spring_mvc.controller;
 
+import java.time.LocalTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -11,6 +15,21 @@ public class HomeController {
     @GetMapping("/")
     public String Greetings() {
         return "greetings";
+    }
+
+    @GetMapping("/home")
+    public String Home() {
+        return "home";
+    }
+
+    @GetMapping("/utente")
+    public String Utente(Model model, @RequestParam(name = "name") String name) {
+        model.addAttribute("name", name);
+        model.addAttribute("surname", "Coniglione");
+
+        model.addAttribute("time", LocalTime.now());
+
+        return "utente";
     }
 
 }
